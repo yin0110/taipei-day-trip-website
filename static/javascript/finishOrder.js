c42f15279e40b28c4e1a04d6b98b1255b1f1368b
Yin Chuang <blackbest_0110@hotmail.com>
2022-04-08 15:00:48 +0800

    oder view update

--- static/javascript/finishOrder.js
+++ static/javascript/finishOrder.js
@@ -3,6 +3,8 @@ let welcomeTitle = document.querySelector(".section--bookingperson");
 let nobooking = document.querySelector(".nobooking");
 let footer = document.querySelector(".footer");
 let customerName = localStorage.getItem("customerName");
+let orderDate = document.querySelector(".bookingperson--orderDate");
+let blank = document.querySelector(".blank");
 
 function getPageOrderNumber() {
 	let ip = location.host;
@@ -17,14 +19,28 @@ async function finishOrder() {
 	let fetchInfo = await fetch(urlForOrder, { method: methodForOrder });
 	let orderInfo = await fetchInfo.json();
 	if (orderInfo["data"]) {
+		setTimeout(function () {
+			blank.style.display = "none";
+		}, 300);
 		welcomeTitle.innerHTML = `您好，${customerName}，感謝您完成訂購`;
 		nobooking.innerHTML = `訂單號碼：${orderNumber}`;
 		welcomeTitle.style.marginBottom = "35px";
-		nobooking.style.marginBottom = "40px";
+		nobooking.style.marginBottom = "20px";
+		orderDate.innerHTML =
+			"訂購日期：" +
+			orderNumber.slice(0, 4) +
+			"-" +
+			orderNumber.slice(4, 6) +
+			"-" +
+			orderNumber.slice(6, 8);
 	} else {
+		setTimeout(function () {
+			blank.style.display = "none";
+		}, 300);
 		welcomeTitle.innerHTML = `您好，請輸入正確的訂單號碼`;
 		welcomeTitle.style.marginBottom = "35px";
 		nobooking.style.marginBottom = "40px";
+		orderDate.style.display = "none";
 	}
 }
 finishOrder();
